55a3b4bf98a589b98e06ee0eb66a54d51d6a7f4f
Nate Armstrong <nate@custombit.com>
2014-03-19 23:07:43 -0600

    Styles login screen. Adds api key help dialog.

--- app/src/main/java/com/example/app/LoginActivity.java
+++ app/src/main/java/com/example/app/LoginActivity.java
@@ -1,5 +1,8 @@
 package com.example.app;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Typeface;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -10,6 +13,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class LoginActivity extends ActionBarActivity {
 
@@ -49,7 +56,9 @@ public class LoginActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class PlaceholderFragment extends Fragment implements View.OnClickListener {
+
+        private AlertDialog apiKeyHelpDialog;
 
         public PlaceholderFragment() {
         }
@@ -58,7 +67,43 @@ public class LoginActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+
+            TextView logoText = (TextView) rootView.findViewById(R.id.logo_text);
+            TextView helpText = (TextView) rootView.findViewById(R.id.help_text);
+            EditText apiKey = (EditText) rootView.findViewById(R.id.api_key);
+            Button button = (Button) rootView.findViewById(R.id.button);
+            ImageView apiKeyHelp = (ImageView) rootView.findViewById(R.id.api_key_help);
+
+            Typeface extraBold = Typeface.createFromAsset(getActivity().getAssets(), "Proxima_Nova_Extrabold.ttf");
+            logoText.setTypeface(extraBold);
+            helpText.setTypeface(extraBold);
+            apiKey.setTypeface(extraBold);
+            button.setTypeface(extraBold);
+
+            createApiHelpDialog();
+
+            apiKeyHelp.setOnClickListener(this);
             return rootView;
+        }
+
+        private void createApiHelpDialog() {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage(R.string.api_help_dialog_message)
+                    .setTitle(R.string.api_help_dialog_title)
+                    .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // User clicked OK button
+                        }
+                    });
+            apiKeyHelpDialog = builder.create();
+        }
+
+        // onclick
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.api_key_help:
+                    apiKeyHelpDialog.show();
+            }
         }
     }
 
