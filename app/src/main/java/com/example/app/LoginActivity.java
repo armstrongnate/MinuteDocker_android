ba68c2a247eeae42515766c1534cd0c4b539939d
Nate Armstrong <nate@custombit.com>
2014-03-21 23:57:28 -0600

    Request current account from minutedock using api key and store api key and current account id in shared preferences.

--- app/src/main/java/com/example/app/LoginActivity.java
+++ app/src/main/java/com/example/app/LoginActivity.java
@@ -1,28 +1,44 @@
 package com.example.app;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class LoginActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
 
         if (savedInstanceState == null) {
@@ -59,6 +75,9 @@ public class LoginActivity extends ActionBarActivity {
     public static class PlaceholderFragment extends Fragment implements View.OnClickListener {
 
         private AlertDialog apiKeyHelpDialog;
+        protected EditText apiKey;
+        protected SharedPreferences prefs;
+        public static final String TAG = LoginActivity.class.getSimpleName();
 
         public PlaceholderFragment() {
         }
@@ -68,16 +87,23 @@ public class LoginActivity extends ActionBarActivity {
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_login, container, false);
 
+            MinuteDockr app = MinuteDockr.getInstance(getActivity());
+            prefs = app.sharedPreferences;
+            String apiKeyString = prefs.getString(app.API_KEY_PREFS_KEY, "");
+
             TextView logoText = (TextView) rootView.findViewById(R.id.logo_text);
             TextView helpText = (TextView) rootView.findViewById(R.id.help_text);
-            EditText apiKey = (EditText) rootView.findViewById(R.id.api_key);
+            apiKey = (EditText) rootView.findViewById(R.id.api_key);
+            apiKey.setText(apiKeyString);
             Button button = (Button) rootView.findViewById(R.id.button);
+            button.setOnClickListener(this);
             ImageView apiKeyHelp = (ImageView) rootView.findViewById(R.id.api_key_help);
 
             Typeface extraBold = Typeface.createFromAsset(getActivity().getAssets(), "Proxima_Nova_Extrabold.ttf");
+            Typeface semiBold = Typeface.createFromAsset(getActivity().getAssets(), "Proxima_Nova_Semibold.ttf");
             logoText.setTypeface(extraBold);
             helpText.setTypeface(extraBold);
-            apiKey.setTypeface(extraBold);
+            apiKey.setTypeface(semiBold);
             button.setTypeface(extraBold);
 
             createApiHelpDialog();
@@ -88,8 +114,9 @@ public class LoginActivity extends ActionBarActivity {
 
         private void createApiHelpDialog() {
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            builder.setMessage(R.string.api_help_dialog_message)
-                    .setTitle(R.string.api_help_dialog_title)
+            View helpDialog = MinuteDockr.getInstance(getActivity())
+                    .customDialogView(R.string.api_help_dialog_title, R.string.api_help_dialog_message);
+            builder.setView(helpDialog)
                     .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int id) {
                             // User clicked OK button
@@ -103,6 +130,75 @@ public class LoginActivity extends ActionBarActivity {
             switch (v.getId()) {
                 case R.id.api_key_help:
                     apiKeyHelpDialog.show();
+
+                case R.id.button:
+                    if (isNetworkAvailable()) {
+                        GetCurrentAccountTask getAccountTask = new GetCurrentAccountTask();
+                        getAccountTask.execute();
+                    }
+                    else {
+                        // show network error alert dialog
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                        View helpDialog = MinuteDockr.getInstance(getActivity())
+                                .customDialogView(R.string.no_internet_dialog_title, R.string.no_internet_dialog_message);
+                        builder.setView(helpDialog)
+                                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        // User clicked OK button
+                                    }
+                                });
+                        AlertDialog alert = builder.create();
+                        alert.show();
+                    }
+            }
+        }
+
+        private boolean isNetworkAvailable() {
+            ConnectivityManager manager = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+
+            return networkInfo != null && networkInfo.isConnected();
+        }
+
+        public class GetCurrentAccountTask extends AsyncTask<Object, Void, String> {
+
+            @Override
+            protected String doInBackground(Object... arg0) {
+                int responseCode = -1;
+                try {
+                    Log.i(TAG, "api key: " + apiKey.getText().toString());
+                    URL currentAccountUrl = new URL("https://minutedock.com/api/v1/accounts/current.json?api_key=" + apiKey.getText().toString());
+                    HttpURLConnection connection = (HttpURLConnection) currentAccountUrl.openConnection();
+                    connection.connect();
+
+                    responseCode = connection.getResponseCode();
+                    if (responseCode == HttpURLConnection.HTTP_OK) {
+                        MinuteDockr app = MinuteDockr.getInstance(getActivity());
+                        prefs.edit().putString(app.API_KEY_PREFS_KEY, apiKey.getText().toString()).commit();
+
+                        InputStream inputStream = connection.getInputStream();
+                        Reader reader = new InputStreamReader(inputStream);
+                        int contentLength = connection.getContentLength();
+                        char[] charArray = new char[contentLength];
+                        reader.read(charArray);
+                        String responseData = new String(charArray);
+
+                        JSONObject jsonResponse = new JSONObject(responseData);
+                        Log.i(TAG, "Response id: " + jsonResponse.getInt("id"));
+                        prefs.edit().putInt(app.CURRENT_ACCOUNT_ID_PREFS_KEY, jsonResponse.getInt("id")).commit();
+                    }
+                }
+                catch (MalformedURLException e) {
+                    Log.e(TAG, "Exception caught: ", e);
+                }
+                catch (IOException e) {
+                    Log.e(TAG, "Exception caught: ", e);
+                }
+                catch (Exception e) {
+                    Log.e(TAG, "Exception caught: ", e);
+                }
+
+                return "Code: " + responseCode;
             }
         }
     }
---
+++ app/src/main/java/com/example/app/MinuteDockr.java
@@ -0,0 +1,48 @@
+package com.example.app;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by nate on 3/20/14.
+ */
+public class MinuteDockr {
+    private static final String PREFS_FILE = "minute_dockr";
+    public static final String CURRENT_ACCOUNT_ID_PREFS_KEY = "current_account_id";
+    public static final String API_KEY_PREFS_KEY = "api_key";
+    private static final String MY_API_KEY = "0e3ec0f390e9b7aff763d64d8cea6c50";
+    private static MinuteDockr instance = null;
+    public SharedPreferences sharedPreferences;
+    public Context context;
+
+    private MinuteDockr(Context appContext) {
+        context = appContext;
+        sharedPreferences = context.getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE);
+    }
+
+    public static MinuteDockr getInstance(Context context) {
+        if (instance == null) {
+            instance = new MinuteDockr(context);
+        }
+        return instance;
+    }
+
+    public View customDialogView(int titleTextId, int messageTextId) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View customDialog = inflater.inflate(R.layout.md_custom_dialog, null);
+        TextView title = (TextView) customDialog.findViewById(R.id.title);
+        title.setText(titleTextId);
+        TextView message = (TextView) customDialog.findViewById(R.id.message);
+        message.setText(messageTextId);
+        Typeface extraBold = Typeface.createFromAsset(context.getAssets(), "Proxima_Nova_Extrabold.ttf");
+        Typeface semiBold = Typeface.createFromAsset(context.getAssets(), "Proxima_Nova_Semibold.ttf");
+        title.setTypeface(extraBold);
+        message.setTypeface(semiBold);
+
+        return customDialog;
+    }
+}
