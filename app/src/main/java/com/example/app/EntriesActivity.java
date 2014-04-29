984b16e3968ddf2f0300882f00773afdf5e87596
Nate Armstrong <nate@custombit.com>
2014-04-28 22:09:23 -0600

    Removes unnecessary code

--- app/src/main/java/com/example/app/EntriesActivity.java
+++ app/src/main/java/com/example/app/EntriesActivity.java
@@ -61,9 +61,6 @@ public class EntriesActivity extends ActionBarActivity {
 
   @Override
   public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
     int id = item.getItemId();
     if (id == R.id.action_settings) {
       return true;
@@ -123,23 +120,6 @@ public class EntriesActivity extends ActionBarActivity {
     @Override
     public Fragment getItem(int position) {
       return EntriesFragment.newInstance(position);
-    }
-
-    @Override
-    public CharSequence getPageTitle(int position) {
-      switch (position) {
-        case 0: {
-          return "Today";
-        }
-        case 1: {
-          return "Week";
-        }
-        case 2: {
-          return "Month";
-        }
-        default:
-          return null;
-      }
     }
   }
 
